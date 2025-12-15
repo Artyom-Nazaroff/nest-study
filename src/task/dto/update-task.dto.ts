a15d59f0c9cd6acc65cec407d0588e514e711c43
Artyom Nazaroff <artyom.nazaroff@yandex.ru>
2025-12-15 10:47:33 +0700

    Finish create-task dto

--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class UpdateTaskDto {
+  @IsString({ message: 'Hey dude it must be a string!' })
+  @IsNotEmpty({ message: 'Hey dude it can not be empty yopta!' })
+  @Length(2, 40, { message: 'The name must be from 2 till 40 symbols maaaan!' })
   title: string;
+
+  @IsBoolean({ message: 'It must be boolean yooo!' })
   isCompleted: boolean;
 }
