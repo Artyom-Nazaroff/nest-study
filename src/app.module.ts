d21ee95731b65d4547512da6f8721785dbdc795f
Artyom Nazaroff <artyom.nazaroff@yandex.ru>
2026-01-09 15:45:04 +0700

    Start project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,16 @@
-import {
-  Module,
-  type MiddlewareConsumer,
-  type NestModule,
-} from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MovieModule } from './movie/movie.module';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ReviewModule } from './review/review.module';
-import { ActorModule } from './actor/actor.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { LoggingMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     PrismaModule,
-    MovieModule,
-    ReviewModule,
-    ActorModule,
     PrismaModule,
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { AllExceptionFilter } from './common/filters/all-exceptions.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalFilters(new AllExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Nest Course API')
